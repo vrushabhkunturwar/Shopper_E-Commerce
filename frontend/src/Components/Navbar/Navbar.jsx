b3d5a701279de87c8d7d8673ff0a46f9caf5b4a4
vrushabhkunturwar <vrushabhkunturwar@gmail.com>
2024-04-04 18:01:20 +0530

    NavBar Created

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+
+export const Navbar = () => {
+const [menu, setMenu]= useState("shop"); 
+
+  return (
+    <div className='navbar'>
+        <div className='nav-logo'>
+            <img src={logo} alt="logo" />
+            <p>SHOPPER</p>
+        </div>
+        <ul className='nav-menu'>
+            <li onClick = {()=>{setMenu("shop")}}>Shop {menu==="shop"?<hr/>:<></> }</li>
+            <li onClick = {()=>{setMenu("mens")}}>Men {menu==="mens"?<hr/> :<></> }</li>
+            <li onClick = {()=>{setMenu("womens")}}>Women {menu==="womens"?<hr/> :<></> }</li>
+            <li onClick = {()=>{setMenu("kids")}}>Kids {menu==="kids"?<hr/> :<></> }</li>
+        </ul>
+        <div className='nav-logo-cart'>
+            <button>Login</button>
+            <img src={cart_icon} alt="Cart-icon" />
+            <div className='nav-cart-count'>
+                0
+            </div>
+        </div>
+    </div>
+  )
+}
